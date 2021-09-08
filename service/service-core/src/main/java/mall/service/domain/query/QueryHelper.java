5f2199cdf606ef23e87c09c86ba96e168c1c198e
hongmiao.yu <hongmiao.yu@mtime.com>
2021-09-08 16:10:43 +0800

    Optimize QueryHelper

--- service/service-core/src/main/java/mall/service/domain/query/QueryHelper.java
+++ service/service-core/src/main/java/mall/service/domain/query/QueryHelper.java
@@ -6,10 +6,10 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
 import java.beans.PropertyDescriptor;
+import java.time.temporal.Temporal;
 import java.util.List;
 
-import static mall.service.domain.query.Criteria.equalTo;
-import static mall.service.domain.query.Criteria.in;
+import static mall.service.domain.query.Criteria.*;
 
 /**
  * orderBy=name^desc
@@ -44,7 +44,26 @@ public class QueryHelper {
                 builder.pageIndex((int) value);
             } else if ("pageSize".equals(name)) {
                 builder.pageSize((int) value);
-            } else if (value instanceof String || value instanceof Number) {
+            } else if (value instanceof String) {
+                String valueStr = (String) value;
+                if (valueStr.startsWith(">")) {
+                    if (valueStr.startsWith("=", 1)) {
+                        criteria = criteria.and(greaterThanOrEqualTo(name, valueStr.substring(2)));
+                    } else {
+                        criteria = criteria.and(greaterThen(name, valueStr.substring(1)));
+                    }
+                } else if (valueStr.startsWith("<")) {
+                    if (valueStr.startsWith("=", 1)) {
+                        criteria = criteria.and(lessThanOrEqualTo(name, valueStr.substring(2)));
+                    } else {
+                        criteria = criteria.and(lessThan(name, valueStr.substring(1)));
+                    }
+                } else {
+                    criteria = criteria.and(equalTo(name, value));
+                }
+            } else if (value instanceof Number) {
+                criteria = criteria.and(equalTo(name, value));
+            } else if (value instanceof Temporal) {
                 criteria = criteria.and(equalTo(name, value));
             } else if (value instanceof List) {
                 criteria = criteria.and(in(name, (List<?>) value));
@@ -53,7 +72,8 @@ public class QueryHelper {
             } else {
                 log.warn("Unsupported property type {}, property {} ignored!", pd.getPropertyType(), name);
             }
+
         }
-        return builder.build();
+        return builder.criteria(criteria).build();
     }
 }
