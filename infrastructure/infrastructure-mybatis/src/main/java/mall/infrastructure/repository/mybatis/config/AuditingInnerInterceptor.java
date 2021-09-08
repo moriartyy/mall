c2b53736621a333eb2385b14027517ec91f2a6d2
hongmiao.yu <hongmiao.yu@mtime.com>
2021-09-08 22:08:07 +0800

    init entity when parameter is a map

--- infrastructure/infrastructure-mybatis/src/main/java/mall/infrastructure/repository/mybatis/config/AuditingInnerInterceptor.java
+++ infrastructure/infrastructure-mybatis/src/main/java/mall/infrastructure/repository/mybatis/config/AuditingInnerInterceptor.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 /**
  * @author walter
@@ -14,15 +15,25 @@ public class AuditingInnerInterceptor implements InnerInterceptor {
 
     public void beforeUpdate(Executor executor, MappedStatement ms, Object parameter) throws SQLException {
         if (parameter instanceof AuditableEntity) {
-            AuditableEntity<?> auditableEntity = (AuditableEntity<?>) parameter;
-            switch (ms.getSqlCommandType()) {
-                case INSERT:
-                    auditableEntity.beforeInsert();
-                    break;
-                case UPDATE:
-                    auditableEntity.beforeUpdate();
-                    break;
-            }
+            prepareEntity(ms, (AuditableEntity<?>) parameter);
+        } else if (parameter instanceof Map) {
+            Map<?, ?> parameterMap = (Map<?, ?>) parameter;
+            parameterMap.values().forEach(p -> {
+                if (p instanceof AuditableEntity) {
+                    prepareEntity(ms, (AuditableEntity<?>) p);
+                }
+            });
+        }
+    }
+
+    private void prepareEntity(MappedStatement ms, AuditableEntity<?> auditableEntity) {
+        switch (ms.getSqlCommandType()) {
+            case INSERT:
+                auditableEntity.beforeInsert();
+                break;
+            case UPDATE:
+                auditableEntity.beforeUpdate();
+                break;
         }
     }
 }
